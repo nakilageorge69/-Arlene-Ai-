151e7e3941ad39ef16f4e32c948b30c35d7746c8
Mark <123864853+Jhon-mark23@users.noreply.github.com>
2025-01-13 22:46:26 +0800

    Create rwords.js

---
+++ cmds/rwords.js
@@ -0,0 +1,26 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/message');
+
+module.exports = {
+  name: "rwords",
+  description: "Send a random  words",
+  author: "mark",
+
+  async execute(senderId, args, pageAccessToken) {
+    try {
+      const response = await axios.get('https://random-hugot-api.onrender.com/random');
+      const { link: random, author, } = response.data;
+
+      await sendMessage(senderId, {
+        text: ` 🤣Random: ${random}\n🤣 author: ${author}\n`
+      }, pageAccessToken);
+
+      
+    } catch (error) {
+      console.error("nag error chat mo si owner:", error);
+      sendMessage(senderId, {
+        text: `nag error chat mo si owner. Error: ${error.message || "Unknown error"}`
+      }, pageAccessToken);
+    }
+  }
+};
