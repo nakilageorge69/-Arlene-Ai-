60cc472c41592deafd6bb74fe995d04cc0e64d86
Mark <123864853+Jhon-mark23@users.noreply.github.com>
2025-01-15 22:35:26 +0800

    Create cdp.js

---
+++ cmds/cdp.js
@@ -0,0 +1,41 @@
+const axios = require('axios');
+const { sendMessage } = require('../handles/message');
+
+module.exports = {
+  name: "cdp",
+  description: "Send a couple dp",
+  author: "mark",
+
+  async execute(senderId, args, pageAccessToken) {
+    try {
+      const response = await axios.get('https://rest-api-bot.onrender.com/api/cdp');
+      const { result: one, two, } = response.data;
+
+      
+
+      await sendMessage(senderId, {
+        attachment: {
+          type: "photo. ",
+          payload: {
+            url: one
+          }
+        }
+      }, pageAccessToken);
+      
+      await sendMessage(senderId, {
+        attachment: {
+          type: "photo",
+          payload: {
+            url: two
+          }
+        }
+      }, pageAccessToken);
+      
+    } catch (error) {
+      console.error("nag error chat mo si owner:", error);
+      sendMessage(senderId, {
+        text: `nag error chat mo si owner. Error: ${error.message || "Unknown error"}`
+      }, pageAccessToken);
+    }
+  }
+};
