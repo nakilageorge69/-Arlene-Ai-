d0e7d9965154d501b2bb0aee2e49561410de052c
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-01-21 18:33:55 +0800

    Update gmage.js

--- cmds/gmage.js
+++ cmds/gmage.js
@@ -5,18 +5,19 @@ module.exports = {
   name: 'gmage',
   description: 'Generate an image based on a prompt.',
   role: 1,
-  author: 'GeoDevz69',
+  author: 'Mark and GeoDevz69',
   
   async execute(senderId, args, pageAccessToken) {
     if (!args || args.length === 0) {
       await sendMessage(senderId, {
-        text: 'Please provide a prompt.\n\nUsage:\nExample: gmage hotdogs'
+        text: 'Please provide a prompt.\n\nUsage:\nExample: gen cat or dog'
       }, pageAccessToken);
       return;
     }
 
     const prompt = args.join(' ');
-    const apiUrl = `https://zaikyoo.onrender.com/api/art1?prompt=${encodeURIComponent(prompt)}`;
+    const apiUrl = `https://zaikyoo.onrender.com/api/artv1?p=${encodeURIComponent(prompt)}`;
+    
 
     try {
       await sendMessage(senderId, {
@@ -36,4 +37,3 @@ module.exports = {
     }
   }
 };
-    
