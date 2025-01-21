c23c22184aa51ff0a7980585adbc9cd5429decd6
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-01-21 11:37:11 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -32,7 +32,7 @@ module.exports = {
 
     // Display all commands if "help all" is provided
     if (args[0]?.toLowerCase() === 'all') {
-      const helpTextMessage = `╭── 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 ────⌬\n | [ Total Commands : ${totalCommands} ]\n | ${commands.map((cmd, index) => ` \n | ${index + 1}. ${cmd.title}\n | ○ ${cmd.description}\n |`).join('')}\n | ••••[ Owner: GeoDevz69 ]••••\n╰────────────⌬`;
+      const helpTextMessage = `╭── 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 ────⌬\n | [ Total Commands : ${totalCommands} ]\n | ${commands.map((cmd, index) => ` \n | ${index + 1}. ${cmd.title}\n | ○ ${cmd.description}\n |`).join('')}\n | ••••[ Owner: GeoDevz69 ]••••\n╰──────────────⌬`;
       return sendMessage(senderId, { text: helpTextMessage }, pageAccessToken);
     }
 
@@ -45,7 +45,7 @@ module.exports = {
       }, pageAccessToken);
     }
 
-    const helpTextMessage = `╭─── 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 ──⌬\n | [ Page View ${page}/${totalPages} ]\n | [ Total Commands : ${totalCommands} ]\n |\n${commandsForPage.map((cmd, index) => `${startIndex + index + 1}. ${cmd.title}\n | ○ ${cmd.description}\n |`).join('')}\n\n | 🔕 Note : Use "help [page]" to switch pages, or "help all" to see all commands!\n╰────────────⌬`;
+    const helpTextMessage = `╭─── 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 ──⌬\n | [ Page View ${page}/${totalPages} ]\n | [ Total Commands : ${totalCommands} ]\n |\n${commandsForPage.map((cmd, index) => ` \n | ${startIndex + index + 1}. ${cmd.title}\n | ○ ${cmd.description}\n |`).join('')}\n |\n | 🔕 Note : Use "help [page]" to switch pages, or "help all" to see all commands!\n╰──────────────⌬`;
 
 
     const quickReplies = commandsForPage.map((cmd) => ({
@@ -61,4 +61,3 @@ module.exports = {
     }, pageAccessToken);
   }
 };
-                        
