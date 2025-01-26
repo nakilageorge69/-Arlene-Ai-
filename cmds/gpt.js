712f1b7440af4910c6ce3dd514c3d0ce2b8ddc3f
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-01-26 09:49:27 +0800

    Update gpt.js

--- cmds/gpt.js
+++ cmds/gpt.js
@@ -5,7 +5,7 @@ module.exports = {
   name: 'blackbox',
   description: 'Ask a question to the Blackbox AI',
   role: 1,
-  author: 'Kiana',
+  author: 'mark Martinez',
 
   async execute(senderId, args, pageAccessToken) {
     const prompt = args.join(' ').trim();
@@ -16,8 +16,25 @@ module.exports = {
         text: 'Hello! I am Blackbox Ai, how can I help you?'
       }, pageAccessToken);
     }
+     //api
+    //https://kaiz-apis.gleeze.com/api/blackbox?q=&uid=1
 
-    const apiUrl = `https://clarence-rest-apiv1.onrender.com/api/blackbox?message=${encodeURIComponent(prompt)}`;
+   //before 
+    //const apiUrl = `https://clarence-rest-apiv1.onrender.com/api/blackbox?message=${encodeURIComponent(prompt)}`;
+    
+    //after
+     const apiUrl = `https://kaiz-apis.gleeze.com/api/blackbox?q=${encodeURIComponent(prompt)}&uid=12`;
+     
+     //tignan mo yung pinag kaiba ng before and after tas pag may uid sa dulo i dudugtong molang din yon katulad ng after 
+     
+     //pag ang api mo ay iba ang response 
+     //result ang naka lagay yung sa response data babaguhin mo
+     //example 
+     //eto yung nauna 
+     // response.data.response
+     // tas eto naman pag ang api mo ay may naka lagay na result yung response nya
+     //response.data.result
+    
 
     try {
       const response = await axios.get(apiUrl);
@@ -25,7 +42,7 @@ module.exports = {
 
       if (reply) {
         
-        const formattedResponse = `💻📦 𝗕𝗹𝗮𝗰𝗸𝗯𝗼𝘅 𝗔𝗜 𝗥𝗲𝘀𝗽𝗼𝗻𝘀𝗲:\n\n${reply}`;
+        const formattedResponse = `📦 𝗕𝗹𝗮𝗰𝗸𝗯𝗼𝘅 𝗥𝗲𝘀𝗽𝗼𝗻𝘀𝗲:\n\n${reply}`;
         
         
         const maxLength = 2000;
