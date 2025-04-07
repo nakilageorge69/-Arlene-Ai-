c7364f742d268209be0b486ced548861b7569648
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-04-07 15:18:46 +0800

    Update arleneai.js

--- cmds/arleneai.js
+++ cmds/arleneai.js
@@ -36,7 +36,7 @@ module.exports = {
         sendLongMessage(bot, visionResponse, authToken);
       } else {
         // If no image, use GPT API.  https://rest-api-bot.onrender.com/api/chatgpt?query=${encodeURIComponent(finalPrompt)}`;
-        const apiUrl = `https://kaiz-apis.gleeze.com/api/github-copilot?ask=&uid=${encodeURIComponent(finalPrompt)}`;
+        const apiUrl = `https://kaiz-apis.gleeze.com/api/github-copilot?ask=${encodeURIComponent(finalPrompt)}&uid=1`;
         //https://rest-api-french3.onrender.com/api/clarencev2`;
         const response = await axios.get(apiUrl, finalPrompt);
         const gptMessage = response.data.response;
