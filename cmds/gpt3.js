97c47e848552be1954c55c3a2bad5f1ca68a5765
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-04-07 13:43:36 +0800

    Update gpt3.js

--- cmds/gpt3.js
+++ cmds/gpt3.js
@@ -17,7 +17,7 @@ module.exports = {
       }, pageAccessToken);
     }
 
-    const apiUrl = `https://api.zetsu.xyz/api/gpt-3.5-turbo?uid=1&prompt=hi${encodeURIComponent(prompt)}`;
+    const apiUrl = `https://api.zetsu.xyz/api/gpt-3.5-turbo?prompt=${encodeURIComponent(prompt)}&uid=1`;
 
     try {
       const response = await axios.get(apiUrl);
