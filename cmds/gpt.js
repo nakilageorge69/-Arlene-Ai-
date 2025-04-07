f9c89735418001ce3444ce37896a8d07ffc31326
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-04-07 14:41:52 +0800

    Update gpt.js

--- cmds/gpt.js
+++ cmds/gpt.js
@@ -38,7 +38,7 @@ module.exports = {
 
     try {
       const response = await axios.get(apiUrl);
-      const reply = response.data.response;
+      const reply = response.data.content;
 
       if (reply) {
         
