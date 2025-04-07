d1020f21630950089bbff37c81b47fc23e7dec09
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-04-07 21:25:00 +0800

    Update spotifypro.js

--- cmds/spotifypro.js
+++ cmds/spotifypro.js
@@ -17,7 +17,7 @@ module.exports = {
     }
 
     try {
-      const res = await axios.get('https://api.zetsu.xyz/search/spotify?q=', {
+      const res = await axios.get('https://kaiz-apis.gleeze.com/api/spotify-search?q=', {
         params: { search: searchQuery }
       });
 
