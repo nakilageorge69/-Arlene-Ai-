c4ed0d886917013072e8806e023ecd1b785a7451
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-05-03 19:03:10 +0800

    Update chatgpt.js

--- cmds/chatgpt.js
+++ cmds/chatgpt.js
@@ -23,7 +23,7 @@ module.exports = {
     //const apiUrl = `https://clarence-rest-apiv1.onrender.com/api/blackbox?message=${encodeURIComponent(prompt)}`;
     
     //after
-     const apiUrl = `https://kaiz-apis.gleeze.com/api/catgpt?ask=&uid=${encodeURIComponent(prompt)}`;
+     const apiUrl = `https://betadash-api-swordslush-production.up.railway.app/gpt4?ask=${encodeURIComponent(prompt)}`;
      
      //tignan mo yung pinag kaiba ng before and after tas pag may uid sa dulo i dudugtong molang din yon katulad ng after 
      
@@ -38,7 +38,7 @@ module.exports = {
 
     try {
       const response = await axios.get(apiUrl);
-      const reply = response.data.response;
+      const reply = response.data.content;
 
       if (reply) {
         
