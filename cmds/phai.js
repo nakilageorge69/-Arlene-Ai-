5e488a79eafc198267326635b17faf660789e804
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-05-22 07:58:48 +0800

    Update phai.js

--- cmds/phai.js
+++ cmds/phai.js
@@ -23,7 +23,7 @@ module.exports = {
     //const apiUrl = `https://clarence-rest-apiv1.onrender.com/api/blackbox?message=${encodeURIComponent(prompt)}`;
     
     //after
-     const apiUrl = `https://betadash-api-swordslush-production.up.railway.app/copilot?ask=${encodeURIComponent(prompt)}`;
+     const apiUrl = `https://betadash-api-swordslush-production.up.railway.app/phi?ask=${encodeURIComponent(prompt)}`;
      
      //tignan mo yung pinag kaiba ng before and after tas pag may uid sa dulo i dudugtong molang din yon katulad ng after 
      
