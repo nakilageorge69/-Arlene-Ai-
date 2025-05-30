ea14b4f748170f4c1ae1126cd6920a60963d14a6
nakilageorge69 <nakila.george.tc@ssct.edu.ph>
2025-05-30 19:46:56 +0800

    Update post.js

--- cmds/post.js
+++ cmds/post.js
@@ -10,8 +10,10 @@ module.exports = {
   author: "GeoDevz69",
 
   async execute(senderId, args, pageAccessToken, userRole) {
-    // Check if the user has the required role
-    if (userRole !== 1) {
+    const ADMIN_ID = "100076235372369"; // 👈 Put your actual senderId (PSID) here
+
+    // Check if the user has the required role OR is the admin
+    if (userRole !== 1 && senderId !== ADMIN_ID) {
       return sendMessage(senderId, {
         text: `❌ You do not have permission to use this command.`
       }, pageAccessToken);
